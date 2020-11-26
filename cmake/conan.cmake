92e9e9bff138665c4a323435ea296b4590162129
Lesley Lai <lesley@lesleylai.info>
2020-11-26 06:59:36 -0700

    Initial commit

---
+++ cmake/conan.cmake
@@ -0,0 +1,16 @@
+macro(run_conan)
+    if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+        message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+        file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
+                "${CMAKE_BINARY_DIR}/conan.cmake")
+    endif()
+
+    include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+    conan_add_remote(NAME bincrafters URL
+            https://api.bintray.com/conan/bincrafters/public-conan)
+
+    conan_cmake_run(CONANFILE conanfile.txt
+            BASIC_SETUP CMAKE_TARGETS
+            BUILD missing)
+endmacro()
